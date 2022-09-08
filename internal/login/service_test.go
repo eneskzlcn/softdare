8a1e6495fa125b252147ca431c50072f328dc917
eneskzlcn <enes.kizilcin@gmail.com>
2022-09-08 16:45:46 +0300

    login service tested.

---
+++ internal/login/service_test.go
@@ -0,0 +1,105 @@
+package login_test
+
+import (
+	"context"
+	"github.com/eneskzlcn/softdare/internal/login"
+	mocks "github.com/eneskzlcn/softdare/internal/mocks/login"
+	"github.com/golang/mock/gomock"
+	"github.com/rs/xid"
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/zap"
+	"testing"
+	"time"
+)
+
+func TestNewService(t *testing.T) {
+	t.Run("given nil logger then it should return nil when NewService called", func(t *testing.T) {
+		controller := gomock.NewController(t)
+		mockRepo := mocks.NewMockLoginRepository(controller)
+		service := login.NewService(nil, mockRepo)
+		assert.Nil(t, service)
+	})
+	t.Run("given nil repository then it should return nil when NewService called", func(t *testing.T) {
+		service := login.NewService(zap.NewExample().Sugar(), nil)
+		assert.Nil(t, service)
+	})
+	t.Run("given valid args then it should return new service when NewService called", func(t *testing.T) {
+		controller := gomock.NewController(t)
+		mockRepo := mocks.NewMockLoginRepository(controller)
+		service := login.NewService(zap.NewExample().Sugar(), mockRepo)
+		assert.NotNil(t, service)
+	})
+}
+func TestService_Login(t *testing.T) {
+	controller := gomock.NewController(t)
+	mockRepo := mocks.NewMockLoginRepository(controller)
+	service := login.NewService(zap.NewExample().Sugar(), mockRepo)
+
+	t.Run("given exist user with email then it should return the user without error when Login called", func(t *testing.T) {
+		inp := login.LoginInput{
+			Email:    "found@email.com",
+			Username: new(string),
+		}
+		ctx := context.Background()
+		mockRepo.EXPECT().IsUserExistsByEmail(ctx, inp.Email).Return(true, nil)
+		expectedUser := &login.User{
+			ID:        xid.New().String(),
+			Email:     inp.Email,
+			Username:  *inp.Username,
+			CreatedAt: time.Now(),
+			UpdatedAt: time.Now(),
+		}
+		mockRepo.EXPECT().GetUserByEmail(ctx, inp.Email).Return(expectedUser, nil)
+		user, err := service.Login(ctx, inp)
+		assert.Nil(t, err)
+		assert.Equal(t, user, expectedUser)
+	})
+	t.Run("given not exist user with email and username is nil then should return nil and error", func(t *testing.T) {
+		inp := login.LoginInput{
+			Email:    "found@email.com",
+			Username: nil,
+		}
+		ctx := context.Background()
+		mockRepo.EXPECT().IsUserExistsByEmail(ctx, inp.Email).Return(false, nil)
+		user, err := service.Login(ctx, inp)
+		assert.Nil(t, user)
+		assert.Equal(t, err, login.ErrUserNotFound)
+	})
+	t.Run("given not exist user with email and given a username that already taken then should return nil and error", func(t *testing.T) {
+		username := "valid"
+		inp := login.LoginInput{
+			Email:    "found@email.com",
+			Username: &username,
+		}
+		ctx := context.Background()
+		mockRepo.EXPECT().IsUserExistsByEmail(ctx, inp.Email).Return(false, nil)
+		mockRepo.EXPECT().IsUserExistsByUsername(ctx, username).Return(true, nil)
+		user, err := service.Login(ctx, inp)
+		assert.Nil(t, user)
+		assert.Equal(t, err, login.ErrUsernameAlreadyTaken)
+	})
+	t.Run("given not exist user with email and given a username that not already taken then should return new created user without error", func(t *testing.T) {
+		username := "valid"
+		inp := login.LoginInput{
+			Email:    "found@email.com",
+			Username: &username,
+		}
+		createTime := time.Now()
+		expectedUser := login.User{
+			ID:        "",
+			Email:     inp.Email,
+			Username:  username,
+			CreatedAt: createTime,
+			UpdatedAt: createTime,
+		}
+		ctx := context.Background()
+		mockRepo.EXPECT().IsUserExistsByEmail(ctx, inp.Email).Return(false, nil)
+		mockRepo.EXPECT().IsUserExistsByUsername(ctx, username).Return(false, nil)
+		mockRepo.EXPECT().CreateUser(ctx, gomock.Any()).Return(createTime, nil)
+		user, err := service.Login(ctx, inp)
+		assert.NotNil(t, user)
+		assert.Nil(t, err)
+		expectedUser.ID = user.ID
+		assert.Equal(t, *user, expectedUser)
+	})
+}
