f830665eddee03d7fed5a05352187bb32bcdabf8
eneskzlcn <enes.kizilcin@gmail.com>
2022-09-08 16:59:32 +0300

    home handler show function tested.

--- internal/home/handler_test.go
+++ internal/home/handler_test.go
@@ -1,11 +1,13 @@
 package home_test
 
 import (
+	mux_router "github.com/eneskzlcn/mux-router"
 	"github.com/eneskzlcn/softdare/internal/home"
 	mocks "github.com/eneskzlcn/softdare/internal/mocks/home"
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 	"go.uber.org/zap"
+	"net/http"
 	"testing"
 )
 
@@ -36,4 +38,32 @@ func TestNewHandler(t *testing.T) {
 		handler := home.NewHandler(zap.NewExample().Sugar(), mockRenderer, mockSessionProvider)
 		assert.NotNil(t, handler)
 	})
+}
+
+func TestHandler_Show(t *testing.T) {
+	controller := gomock.NewController(t)
+	mockSessionProvider := mocks.NewMockSessionProvider(controller)
+	mockRenderer := mocks.NewMockRenderer(controller)
+	logger := zap.NewExample().Sugar()
+	handler := home.NewHandler(logger, mockRenderer, mockSessionProvider)
+	router := mux_router.New()
+	router.HandleFunc(http.MethodGet, "/", handler.Show)
+	mockRenderer.EXPECT().RenderTemplate(gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any())
+	server := http.Server{
+		Addr:    ":4300",
+		Handler: router,
+	}
+	go server.ListenAndServe()
+	type UserSessionData struct {
+		Email    string
+		Username string
+	}
+	mockSessionProvider.EXPECT().Exists(gomock.Any(), gomock.Any()).Return(true)
+	mockSessionProvider.EXPECT().Get(gomock.Any(), "user").Return(UserSessionData{Email: "a@s.com", Username: "asdad"})
+
+	req, err := http.NewRequest(http.MethodGet, "http://localhost:4300", nil)
+	assert.Nil(t, err)
+	resp, err := http.DefaultClient.Do(req)
+	assert.Nil(t, err)
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
 }
