b31d72958571d33d8f82c9ef27447992310e24d4
= <=>
2022-09-16 01:56:35 +0300

    post client for rabbitmq adapted to work with post-created event message

--- internal/client/queue/post_client.go
+++ internal/client/queue/post_client.go
@@ -6,20 +6,20 @@ import (
 	"github.com/eneskzlcn/softdare/internal/entity"
 )
 
-func (c *Client) ConsumeIncreasePostCommentCount() {
+func (c *Client) CommentCreatedConsumer() {
 	onReceivedChan := make(chan []byte, 0)
-	go c.client.Consume(onReceivedChan, "increase-post-comment-count-consumer", "increase-post-comment-count")
+	go c.client.Consume(onReceivedChan, "comment-created-consumer", "comment-created")
 	var forever chan struct{}
 	go func() {
 		for d := range onReceivedChan {
-			c.logger.Debug("IncreasePostCommentCountConsumer receieved a message")
-			var message entity.IncreasePostCommentCountMessage
+			c.logger.Debug("IncreasePostCommentCountConsumer received a message")
+			var message entity.CommentCreatedMessage
 			err := json.Unmarshal(d, &message)
 			if err != nil {
 				c.logger.Error("unmarshalling error")
 				continue
 			}
-			_, err = c.service.IncreasePostCommentCount(context.Background(), message.PostID, message.IncreaseAmount)
+			_, err = c.service.IncreasePostCommentCount(context.Background(), message.PostID, 1)
 			if err != nil {
 				c.logger.Error("error on increasing post comment count ", err)
 				//maybe we can add a retry mechanism t
