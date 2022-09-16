207b7d305d90fa348c0e46ea3850ecf4204f6526
= <=>
2022-09-16 05:48:09 +0300

    little refactoring on comments

--- internal/client/queue/post_client.go
+++ internal/client/queue/post_client.go
@@ -12,7 +12,7 @@ func (c *Client) ConsumeCommentCreated() {
 	var forever chan struct{}
 	go func() {
 		for d := range onReceivedChan {
-			c.logger.Debug("Comment created consumer received a message")
+			c.logger.Debug("comment-created-consumer received a message")
 			var message entity.CommentCreatedMessage
 			err := json.Unmarshal(d, &message)
 			if err != nil {
@@ -22,7 +22,6 @@ func (c *Client) ConsumeCommentCreated() {
 			_, err = c.service.AdjustPostCommentCount(context.Background(), message.PostID, 1)
 			if err != nil {
 				c.logger.Error("error on increasing post comment count ", err)
-				//maybe we can add a retry mechanism t
 				continue
 			}
 		}
